8c1ec67af6db568dcf0350b0b8bbc60cf09217df
Clint Scholtisek <cvscholtisek@alaska.edu>
2025-07-20 13:40:02 -0800

    PC scripts to toggle lights

---
+++ lights_on.py
@@ -0,0 +1,17 @@
+import socket
+
+KERFUS_IP = "192.168.40.181"
+SWITCHER_IP = "192.168.40.183"
+ESP_PORT = 7658
+
+def send_meow():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto("meow".encode(), (KERFUS_IP, ESP_PORT))
+
+def lights_on():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto("lights_on".encode(), (SWITCHER_IP, ESP_PORT))
+
+if __name__ == "__main__":
+    send_meow()
+    lights_on()
